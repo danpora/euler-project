6ebf704b0abef164d8e1bc82222d07f80f698f36
danporat <einigo@gmail.com>
2017-05-22 22:54:27 +0300

    e18 solution

---
+++ src/com/euler/Euler20.java
@@ -0,0 +1,31 @@
+package com.euler;
+
+import java.math.BigInteger;
+
+//n! means n × (n − 1) × ... × 3 × 2 × 1
+//
+//        For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+//        and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+//
+//        Find the sum of the digits in the number 100!
+
+public class Euler20 {
+    // Sum all digits of the given number factorial
+    public static long factorialSumDigit(int factorial) {
+        BigInteger disZeroFactorial = EulerUtils.bigFactorialForSum(factorial);
+        System.out.println(disZeroFactorial);
+        long digitSum = 0;
+        // Divide factorial by 10 until 0 is reached
+        while (!disZeroFactorial.toString().equals("0")) {
+            digitSum += Integer.parseInt(disZeroFactorial.mod(new BigInteger("10")).toString());
+            disZeroFactorial = disZeroFactorial.divide(new BigInteger("10"));
+        }
+        return digitSum;
+    }
+
+    public static void main(String[] args) {
+        // Solution for Euler-20 problem
+        System.out.println(factorialSumDigit(100));
+    }
+
+}
